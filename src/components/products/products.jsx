f1485ec48851790f177bfdd4ada7971453ddb442
ahmed <ahmedlearn01@gmail.com>
2021-10-06 23:01:04 -0700

    initial commit

---
+++ src/components/products/products.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Grid from '@material-ui/core'
+
+const products = [
+    {id:1, name:'shoes', description:'running Shoes.'},
+    {id:2, name:'macbook', description:'Apple macbook.'},
+    {id:3, name:'iphone', description:'Iphone mobile.'}
+];
+
+const Products = () =>{
+    <main>
+        <Grid container justify="center" spacing={4}>
+            {
+                products.map((product)=>{
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product />
+                        </Grid>
+                })
+            }
+        </Grid>
+    </main>
+}
+
+export default Products
